36829b39e3d54ed8c619bcc08d922bb8d2bd06e4
Dhruvi Gangani <dagangani@myseneca.ca>
2025-05-09 23:24:18 -0400

    Running and checking the Code

--- ICE1/app/build.gradle.kts
+++ ICE1/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ca.georgiancollege.ice1"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ca.georgiancollege.ice1"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- ICE1/app/src/main/java/ca/georgiancollege/ice1/MainActivity.kt
+++ ICE1/app/src/main/java/ca/georgiancollege/ice1/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,12 +19,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val greetingTextView = findViewById<TextView>(R.id.greetingTextView)
         val greetingButton = findViewById<Button>(R.id.greetingButton)
-        val name = editTextName.text.toString()
-        greetingButton.setOnClickListener {
-            greetingTextView.text = "Hello!"
+        val greetingTextView = findViewById<TextView>(R.id.greetingTextView)
+        val editTextName = findViewById<EditText>(R.id.editTextName)
 
+        greetingButton.setOnClickListener {
+            val name = editTextName.text.toString().trim()
+            if (name.isNotEmpty()) {
+                greetingTextView.text = "Hello, $name!"
+            }
+            else {
+                greetingTextView.text = "Hello, World!"
+            }
         }
 
     }
